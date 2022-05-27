fe947c25674f6ea788483aed64ebbab2a7affdaf
Santiago Lobo <lobo.santiago93@gmail.com>
2022-05-27 20:32:16 -0300

    commit the classes

--- entrega_1/App_Azeroth_Broker/models.py
+++ entrega_1/App_Azeroth_Broker/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellidade = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class Vendedor(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+    Server = models.CharField(max_length=30)
+    stock = models.IntegerField()
+    precio = models.FloatField()
+    
+
+class Moderador(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=30)
+    email = models.EmailField()
+
+
